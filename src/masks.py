f4cb9f0c26599b5a824efae3df34161047a51825
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-06-08 16:07:47 +0300

    'update'

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(card_number: str) -> str | None:
+    """ Функция маскирующая номер карты """
+    if card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[:5]} {card_number[5:7]} ** **** {card_number[12:]}"
+    else:
+        return
+
+
+def get_mask_account(bank_account: str) -> str | None:
+    """Функция маскирующая номер счета"""
+    if bank_account.isdigit() and len(bank_account) == 20:
+        return f"**{bank_account[-4::]}"
+    else:
+        return None
