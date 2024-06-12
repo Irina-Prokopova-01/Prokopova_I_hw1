bfc9a679956b52a0f35eda5644a04516737053b4
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-06-12 22:39:12 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,18 @@ from typing import Iterable
 
 
 def filter_by_state(dict_1: Iterable[list[dict]], state='EXECUTED') -> Iterable[list[dict]]:
+    """Функция сортирующая словари по ключу state"""
     filter_dict = []
     for i in dict_1:
         if i.get('state') == state:
             filter_dict.append(i)
     return filter_dict
+
+
+def sort_by_date(dict_1: Iterable[list[dict]], ascending=True) -> Iterable[list[dict]]:
+    """Функция сортирующая словари по убыванию даты"""
+    sorted_date_list = sorted(dict_1, key=lambda x: x['date'], reverse=ascending)
+    return sorted_date_list
+
+
 
