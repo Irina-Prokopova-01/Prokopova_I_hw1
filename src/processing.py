75b9fd55eb7323139485cb3686585182eb37348f
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-06-14 22:55:14 +0300

    updat README else

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(data_list: list[dict], state: str="EXECUTED") -> list[dict]:
+def filter_by_state(data_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция фильтрующая словари по ключу state"""
     filter_dict = []
     for item in data_list:
@@ -7,7 +7,7 @@ def filter_by_state(data_list: list[dict], state: str="EXECUTED") -> list[dict]:
     return filter_dict
 
 
-def sort_by_date(data_list: list[dict], ascending: bool=False) -> list[dict]:
+def sort_by_date(data_list: list[dict], ascending: bool = False) -> list[dict]:
     """Функция сортирующая словари по дате"""
-    sorted_date_list = sorted(data_list, key=lambda x: x["date"], reverse=ascending)
+    sorted_date_list = sorted(data_list, key=lambda key_date: key_date["date"], reverse=ascending)
     return sorted_date_list
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,20 @@
 from src.masks import get_mask_card_number, get_mask_account
 
+# from typing import Optional
+
 
 def mask_account_card(card: str) -> str:
     """Функция возвращает строку с замаскированным счетом или номером карты"""
-    cards = card.split()
-    number = cards[-1]
-    name = " ".join(cards[:-1])
-    if name == 'Счет':
-        number = get_mask_account(number)
+    if card is None:
+        return "None"
     else:
-        number = get_mask_card_number(number)
+        cards = card.split()
+        number = cards[-1]
+        name = " ".join(cards[:-1])
+        if name == "Счет":
+            number = get_mask_account(number)
+        else:
+            number = get_mask_card_number(number)
     return f"{name} {number}"
 
 
