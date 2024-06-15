f8e96909904add3d90b2a4d8f69fae033b608503
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-06-15 21:52:37 +0300

    correct name, cod 15.06

--- src/widget.py
+++ src/widget.py
@@ -6,17 +6,16 @@ def mask_account_card(card: str) -> str | None:
     cards = card.split()
     number = cards[-1]
     name = " ".join(cards[:-1])
-    if name == 'Счет':
-        number = get_mask_account(number)
-    else:
+    if name != 'Счет':
         number = get_mask_card_number(number)
+    else:
+        number = get_mask_account(number)
     return f"{name} {number}"
 
-print(mask_account_card('Maestro 1596837868705199'))
+
+# print(mask_account_card("Maestro 1596837868705199"))
 
 
-def get_data(date: str) -> str:
+def get_data(date: str) -> str | None:
     """Функция преобразования даты"""
-    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
-
-
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
