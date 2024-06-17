03f947718d06b72f1721a30b7dae04e035226ed8
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-06-17 14:38:37 +0300

    add test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,10 @@
 def get_mask_card_number(card_number: str) -> str | None:
     """Функция маскирующая номер карты"""
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:5]} {card_number[5:7]} ** **** {card_number[12:]}"
-    return None
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    raise 'TypeError'
+
+print(get_mask_card_number('7000792289606361'))
 
 
 def get_mask_account(bank_account: str) -> str | None:
@@ -10,4 +12,4 @@ def get_mask_account(bank_account: str) -> str | None:
     if bank_account.isdigit() and len(bank_account) == 20:
         return f"**{bank_account[-4::]}"
     else:
-        return None
+        raise 'TypeError'
