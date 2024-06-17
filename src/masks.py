4d6a12dad55a0ca8a71c70ce8c6c1563618c216d
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-06-17 23:00:13 +0300

    add test 100%

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,17 @@
 def get_mask_card_number(card_number: str) -> str | None:
-    if type(card_number) != str:
+    if type(card_number) is str:
         raise TypeError
     """Функция маскирующая номер карты"""
     if card_number.isdigit() and len(card_number) == 16:
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
     return None
 
+
 # print(get_mask_card_number('7000792289606361'))
 
 
 def get_mask_account(bank_account: str) -> str | None:
-    if type(bank_account) != str:
+    if type(bank_account) is str:
         raise TypeError
     """Функция маскирующая номер счета"""
     if bank_account.isdigit() and len(bank_account) == 20:
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -18,38 +18,15 @@ def test_filter_by_state(data_input, state, expected):
 
 
 
-#
-# @pytest.fixture
-# def sample_data():
-#     return [
-#         {"state": "EXECUTED", "date": "2022-01-15"},
-#         {"state": "PENDING", "date": "2022-01-10"},
-#         {"state": "EXECUTED", "date": "2022-01-20"},
-#         {"state": "FAILED", "date": "2022-01-05"}
-#     ]
-#
-# @pytest.mark.parametrize("state, expected_result", [
-#     ("EXECUTED", [{"state": "EXECUTED", "date": "2022-01-15"}, {"state": "EXECUTED", "date": "2022-01-20"}]),
-#     ("PENDING", [{"state": "PENDING", "date": "2022-01-10"}]),
-#     ("FAILED", [{"state": "FAILED", "date": "2022-01-05"}]),
-# ])
-# def test_filter_by_state(sample_data, state, expected_result):
-#     assert filter_by_state(sample_data, state) == expected_result
-#
-# @pytest.mark.parametrize("ascending, expected_result", [
-#     (True, list(reversed([
-#         {"state": "FAILED", "date": "2022-01-05"},
-#         {"state": "PENDING", "date": "2022-01-10"},
-#         {"state": "EXECUTED", "date": "2022-01-15"},
-#         {"state": "EXECUTED", "date": "2022-01-20"}
-#     ]))),
-#     (False, list(reversed([
-#         {"state": "EXECUTED", "date": "2022-01-20"},
-#         {"state": "EXECUTED", "date": "2022-01-15"},
-#         {"state": "PENDING", "date": "2022-01-10"},
-#         {"state": "FAILED", "date": "2022-01-05"}
-#     ]))),
-# ])
-#
-# def test_sort_by_date(sample_data, ascending, expected_result):
-#     assert sort_by_date(sample_data, ascending) == expected_result
+@pytest.mark.parametrize('ascending, expected', [
+    (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
+    (True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])])
+
+def test_sort_by_date(data_input, ascending, expected):
+    assert sort_by_date(data_input, ascending) == expected
