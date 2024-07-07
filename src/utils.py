eebe82a411ce9eb8ee33340267e9ffd70cc171c7
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-07-08 02:23:28 +0300

    correct 1

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,6 @@ import os.path
 def get_transactions_json_file(path: str) -> list[dict]:
     """Функция которая принимает на вход путь до JSON-файла, возврящает список словарей с данными о фин. транзакциях"""
     if not os.path.exists(path):
-        # print("Путь до файла не найден")
         return []
     try:
         with open(path, "r", encoding="utf-8") as f:
@@ -14,11 +13,8 @@ def get_transactions_json_file(path: str) -> list[dict]:
                 if isinstance(json_file_transactions, list):
                     return json_file_transactions
                 else:
-                    # print("Ошибка: Файл не содержит список транзакций")
                     return []
             except json.JSONDecodeError:
-                # print("Ошибка декодирования JSON")
                 return []
     except FileNotFoundError:
-        # print("Файл не найден")
         return []
