1a1a21fb70708a670c51d1a9f6fc0e1b1deaab31
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-07-13 20:47:41 +0300

    add info in README.md and upgrade utils.py

--- src/masks.py
+++ src/masks.py
@@ -11,12 +11,12 @@ logger.setLevel(logging.DEBUG)
 def get_mask_card_number(card_number: str) -> str | None:
     logger.info(f'Передан номер карты: {card_number}.')
     if type(card_number) is not str:
-        logger.error(f'Передан не корректный тип данных.')
+        logger.error(f'Передан не корректный тип данных {card_number}.')
         raise TypeError
     """Функция маскирующая номер карты"""
     if card_number.isdigit() and len(card_number) == 16:
-        logger.warning(f'Введеный номер должен состоять из 16 цифр')
-        masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+        logger.warning(f'Введеный номер должен состоять из 16 цифр {card_number}')
+        masked_number = f'{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}'
         logger.debug(f'Замаскированный номер карты: {masked_number}.')
         return masked_number
     return None
@@ -25,11 +25,11 @@ def get_mask_card_number(card_number: str) -> str | None:
 def get_mask_account(bank_account: str) -> str | None:
     logger.info(f"Передан номер счета: {bank_account}.")
     if type(bank_account) is not str:
-        logger.error(f"Передан не корректный тип данных.")
+        logger.error(f"Передан не корректный тип данных {bank_account}.")
         raise TypeError
     """Функция маскирующая номер счета"""
     if bank_account.isdigit() and len(bank_account) == 20:
-        logger.warning(f"Введеный номер должен состоять из 20 цифр")
+        logger.warning(f"Введеный номер должен состоять из 20 цифр {bank_account}")
         masked_number = f"**{bank_account[-4::]}"
         logger.debug(f"Замаскированный номер карты: {masked_number}.")
         return masked_number
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 
+import pandas as pd
+
 from src.external_api import convert_to_rubles
 
 logger = logging.getLogger("utils")
@@ -13,28 +15,59 @@ logger.setLevel(logging.DEBUG)
 
 
 def get_transactions_json_file(path: str) -> list[dict]:
-    """
-    Функция, которая принимает на вход путь до JSON-файла,
-    возвращает список словарей с данными о фин. транзакциях.
-    """
+    """ Функция, которая принимает на вход путь до JSON-файла,
+    возвращает список словарей с данными о фин. транзакциях."""
     if not os.path.exists(path):
         logger.warning(f"Проверте корректность указанного пути {path}.")
         return []
+    if ".json" in path:
+        with open(path, encoding="utf-8") as f:
+            try:
+                json_file_transactions = json.load(f)
+                logger.info(f"Файл {json_file_transactions} прочитан.")
+            except json.JSONDecodeError:
+                logger.error(f"Данные в файле {json_file_transactions} не соответствуют формату JSON.")
+                return []
 
-    with open(path, encoding="utf-8") as f:
-        try:
-            json_file_transactions = json.load(f)
-            logger.info(f"Файл {json_file_transactions} прочитан.")
-        except json.JSONDecodeError:
-            logger.error(f"Данные в файле {json_file_transactions} не соответствуют формату JSON.")
+        if isinstance(json_file_transactions, list):
+            logger.info(f"Данные файла {json_file_transactions} это список.")
+            return json_file_transactions
+        else:
+            logger.warning(f"Данные {json_file_transactions} не является списоком.")
             return []
+    elif ".csv" in path:
+        csv_file_transactions = pd.read_csv("../data/transactions.csv")
+        logger.info(f"Файл {csv_file_transactions} прочитан.")
+        return csv_file_transactions.to_dict(orient="records")
+    elif ".xlsx" in path:
+        xlsx_file_transactions = pd.read_excel("../data/transactions_excel.xlsx")
+        logger.info(f"Файл {xlsx_file_transactions} прочитан.")
+        return xlsx_file_transactions.to_dict(orient="records")
 
-    if isinstance(json_file_transactions, list):
-        logger.info(f"Данные файла {json_file_transactions} это список.")
-        return json_file_transactions
+
+if __name__ == "__main__":
+    # transactions = get_transactions_json_file("../data/operations.json")
+    # if transactions:
+    #     print("Список транзакций:")
+    #     for transaction in transactions:
+    #         print(transaction)
+    # else:
+    #     print("Файл не найден, пустой или содержит некорректный формат.")
+
+    transactions_csv = get_transactions_json_file("../data/transactions.csv")
+    if transactions_csv:
+        print("Список транзакций из CSV-файла:")
+        for transaction in transactions_csv:
+            print(transactions_csv)
     else:
-        logger.warning(f"Данные {json_file_transactions} не является списоком.")
-        return []
+        print("Ошибка при чтении CSV-файла.")
+
+# transactions_excel = get_transactions_json_file("../data/transactions_excel.xlsx")
+# if transactions_excel:
+#     print("\nСписок транзакций из XLSX-файла:")
+#     for transaction in transactions_excel:
+#         print(transaction)
+# else: print("Ошибка при чтении XLSX-файла.")
 
 
 def amount_transaction(transaction: dict) -> float:
@@ -66,4 +99,5 @@ transaction = {
     "operationAmount": {"amount": "8221", "currency": {"name": "USD", "code": "USD"}},
 }
 
-print(amount_transaction(transaction))
+if __name__ == "__main__":
+    print(amount_transaction(transaction))
